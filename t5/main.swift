e505e0d5537a8a77343d6257c8ad65745052d66b
william donner <williamdonner@innersanctum.fios-router.home>
2016-06-06 16:59:04 -0400

    Initial Commit

---
+++ t5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  t5
+//
+//  Created by william donner on 6/6/16.
+//  Copyright © 2016 william donner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
